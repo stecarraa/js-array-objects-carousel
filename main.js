817e55d72dad3aa63c4da303a22f6431cbeceb3c
stecarraa <stefano@caratozzolo.me>
2022-06-29 16:01:39 +0200

    sistemati btn

--- main.js
+++ main.js
@@ -49,3 +49,84 @@ const images = [
     },
 ];
 
+// immagine da cui voglio partire nell'array
+let activeElementIndex = 0;
+
+
+// mi vado a prendere dove visualizzare le img nel dome
+const imgsWrapper = document.querySelector('.carousel-container');
+const imgList = imgsWrapper.children;
+
+
+
+
+
+for(index = 0 ; index < givenImages.length ; index++){
+    // creo l'elemento html di tipo img
+const newImg = document.createElement('img');
+
+// aggiungo le classi che voglio che questo abbia
+newImg.classList.add('w-100', 'rounded');
+
+// cambio l'src dell'immagine => .setAttribute(quale attributo, contenuto dell'attributo)
+newImg.setAttribute('src', givenImages[index]);
+
+//  aggiungo l'immagine creata alla lista
+imgsWrapper.append(newImg);
+
+imgList[activeElementIndex].classList.add('active');
+}
+
+
+
+
+
+
+
+
+
+
+// | prendo il bottone "next"
+const btnNext = document.getElementById('next-button');
+
+btnNext.addEventListener('click', nextImage);
+
+const btnPre = document.getElementById('prev-button')
+
+btnPre.addEventListener('click', prevImage);
+
+function nextImage(){
+    imgList[activeElementIndex].classList.remove('active');
+   
+    // ? scorro di un posto l'elemento attivo
+    activeElementIndex++;
+
+    //  se siamo alla fine dell'array, non uscire ma ricomincia dal primo elemento della lista.
+    if (activeElementIndex === imgList.length){
+        activeElementIndex = 0;
+    }
+
+    // aggiungo la classe active al nuovo elemento attivo
+    imgList[activeElementIndex].classList.add('active');
+
+    console.log('Cliccato! L\'elemento adesso attivo è in posizione: ' + activeElementIndex);
+}
+
+function prevImage(){
+    imgList[activeElementIndex].classList.remove('active');
+   
+    // ? scorro di un posto l'elemento attivo
+    activeElementIndex--;
+
+    //  se siamo alla fine dell'array, non uscire ma ricomincia dal primo elemento della lista.
+    if (activeElementIndex === -1){
+        activeElementIndex = imgList.length - 1;
+    }
+
+    // aggiungo la classe active al nuovo elemento attivo
+    imgList[activeElementIndex].classList.add('active');
+
+    console.log('Cliccato! L\'elemento adesso attivo è in posizione: ' + activeElementIndex);
+}
+
+const timer = setInterval(nextImage, 3000);
